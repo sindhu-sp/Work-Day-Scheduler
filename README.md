2a03e78df152680a10d68c111190b30d64f745a8
Sindhu Pillai <sindhupillai@Sindhus-MacBook-Pro.local>
2021-06-06 12:50:53 -0700

    final changed with README added

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Work-Day-Scheduler
+
+## About this challenge
+This challenge is to create a daily planner, for an employee with a busy schedule.
+This planner helps the user to add important events manage my time effectively.
+
+## In this Challenge
+
+- When the user opens the planner, then the current day is displayed at the top of the calendar.
+
+- When the user scrolls down, then presented with time blocks for standard business hours.
+
+- When the user view the time blocks for that day, then each time block is color-coded to indicate whether it is in the    past, present, or future.
+
+- When user clicks into a time block, then I can enter an event.
+
+- When user click the save button for that time block, then the text for that event is saved in local storage.
+
+- When the user refresh the page, then the saved events persist.
+
+- When the user click on the clear button, it clears the local storage and refreshes the page and the tasks are emptied to start new.
+
+## My Password-Generator Mock up
+
+<img src="./assets/refresh.png" alt="refresh page" height = 300 width= 500 />
+<img src="./assets/clear.png" alt="refresh page" height = 300 width= 500 />
+
+## Technologies Used:
+- HTML
+- CSS
+- jQuery
+- Bootstrap
+
+
+#### Sindhu Pillai
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,59 @@
-// $document.ready(function(){
 // Displaying the date at the top of the page below the heading
-
 var dateEl = moment().format('LLLL');
 $("#currentDay").text(dateEl);
+$(document).ready(function () {
+ 
+    // save button click handler
+    // the selector description is returning every siblings
+    $(".saveBtn").on ("click", function () {
+        var timeEl = $(this).parent().attr("id");
+        var taskInput=$(this).siblings(".description").val();
+        localStorage.setItem(timeEl,taskInput);
+    });
+  
+    // The daily tasks are being are being retrieved from localStorage
+    var toDo = function() {
+        $("#9 .description").val(localStorage.getItem("9"));
+        $("#10 .description").val(localStorage.getItem("10"));
+        $("#11 .description").val(localStorage.getItem("11"));
+        $("#12 .description").val(localStorage.getItem("12"));
+        $("#1 .description").val(localStorage.getItem("1"));
+        $("#2 .description").val(localStorage.getItem("2"));
+        $("#3 .description").val(localStorage.getItem("3"));
+        $("#4 .description").val(localStorage.getItem("4"));
+        $("#5 .description").val(localStorage.getItem("5"));
 
-// The daily tasks are being stored in the localStorage
-var toDo = {
-    "0": [],
-    "1": [],
-    "2": [],
-    "3": [],
-    "4": [],
-    "5": [],
-    "6": [],
-    "7": [],
-    "8": []
+    };
+    toDo();
+    hourEl();
 
-};
+    // defining variables to add color to th past current and future event
+    
+    function hourEl() {
+        var currentHour = moment().hour();
+        $(".time-block").each(function () {
+            var taskHour = parseInt($(this).attr("id"));
+            if (taskHour < 6) {
+                taskHour += 12;
+            }
+            if (taskHour < currentHour) {
+                $(this).removeClass(["present", "future"]).addClass("past");
+                $(this).find(".description").prop("disabled", true);
+            } else if (taskHour === currentHour) {
+                $(this).removeClass(["past", "future"]).addClass("present");
+            } else {
+                $(this).removeClass(["past", "present"]).addClass("future");
+            }
+        })
+    }
+    // when clear button is clicked the local storage is cleared and the page reloaded if the user wants which is optional
+    $(".clearBtn").on ("click", function () {
+        localStorage.clear()
+        location.reload()
+    });
 
-// Storing tasks to local storage
-var saveToDo = function () {
-    localStorage.setItem("toDo", JSON.stringify(toDo));
-}
-
-var currentHour = moment().hour();
-console.log(currentHour);
-
-function hourEl() {
-    // $(".time-block").each(function () {
-    $(".time-block").each(function () {
-        var taskHour = parseInt($(this).attr("id"));
-        if (taskHour < 6) {
-            taskHour += 12;
-        }
-        console.log("@@@@@@@@@@")
-        console.log(currentHour)
-        
-        console.log(taskHour);
-        console.log("@@@@@@@@@@")
-        if (taskHour < currentHour) {
-            $(this).removeClass(["present", "future"]).addClass("past");
-        } else if (taskHour === currentHour) {
-            $(this).removeClass(["past", "future"]).addClass("present");
-        } else {
-            $(this).removeClass(["past", "present"]).addClass("future");
-        }
-    })
-
-    // })
-}
-hourEl();
-// for (var id=0; id< 8; id++){
-//     if(currentHour > id) {
-//     $(".past").eq(id-9);
-// }
-
-
-//     if(currentHour === id){
-//     $(".prsent").eq(id-9);
-//     }
-//     if(currentHour < id){
-//         $(".future").eq(id-9);
-//         }
-// }
-
-
-// The textarea element is replaced with another element and stored in the localStorage
-// Click funtion saving the data
-// if the time is future then the textarea element could be updated
-
-// Input element is created 
-// the input is appended to parent element
-// Check the time left until the upcoming hour
-// });
+    // when the refresh button is clicked the added task persists
+    $("refreshBtn").on ("click", function () {
+        location.reload()
+    });
+});
